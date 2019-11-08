7a99a43958cdaa129d014ac215950aaeb221fd4a
Bernardo Ramos <bernardo@blocko.io>
2019-11-08 20:23:41 -0300

    test: update db functions

--- test/test.c
+++ test/test.c
@@ -1,94 +1,159 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "sqlite3.h"
 #include "assert.h"
 
 sqlite3 *db;
 
-struct product {
-  char   name[32];
-  int    qty;
-  double price;
-};
+#define QUIT_TEST()  exit(1)
 
-typedef struct product product;
+/****************************************************************************/
 
-void db_execute(char *cmd){
+int bind_sql_parameters(sqlite3_stmt *stmt, const char *types, va_list ap){
+  int rc, parameter_count, iDest, i;
+  char c;
+
+  parameter_count = sqlite3_bind_parameter_count(stmt);
+
+  iDest = 1;
+  for(i=0; (c = types[i])!=0; i++){
+    if( i+1>parameter_count ) goto loc_invalid;
+    if( c=='s' ){
+      char *z = va_arg(ap, char*);
+      sqlite3_bind_text(stmt, iDest+i, z, -1, SQLITE_TRANSIENT);
+    }else if( c=='i' ){
+      sqlite3_bind_int64(stmt, iDest+i, va_arg(ap, int));
+    }else if( c=='l' ){
+      sqlite3_bind_int64(stmt, iDest+i, va_arg(ap, sqlite_int64));
+    //}else if( c=='f' ){  -- floats are promoted to double
+    //  sqlite3_bind_double(stmt, iDest+i, va_arg(ap, float));
+    }else if( c=='d' ){
+      sqlite3_bind_double(stmt, iDest+i, va_arg(ap, double));
+    }else if( c=='b' ){
+      char *ptr = va_arg(ap, char*);
+      int size = va_arg(ap, int);
+      sqlite3_bind_blob(stmt, iDest+i, ptr, size, SQLITE_TRANSIENT);
+      iDest++;
+    }else if( c=='@' ){
+      /* do not bind value at this position */
+    }else{
+      goto loc_invalid;
+    }
+  }
+
+  return SQLITE_OK;
+
+loc_invalid:
+  return SQLITE_MISUSE;
+}
+
+/****************************************************************************/
+
+void print_error(int rc, char *desc, char *sql, const char *function, int line){
+
+  printf("\n\tFAIL %d: %s\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", rc, desc, sql, function, line);
+
+}
+
+/****************************************************************************/
+
+void db_execute_fn(sqlite3 *db, char *sql, const char *function, int line){
   char *zErrMsg=0;
   int rc;
 
-  rc = sqlite3_exec(db, cmd, 0, 0, &zErrMsg);
+  rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
   if( rc!=SQLITE_OK ){
-    printf("FAIL %d: %s\n\tsql: %s\n", rc, zErrMsg, cmd);
+    print_error(rc, zErrMsg, sql, function, line);
     sqlite3_free(zErrMsg);
-    exit(1);
+    QUIT_TEST();
   }
 
 }
 
-void db_catch(char *cmd){
+#define db_execute(sql) db_execute_fn(db, sql, __FUNCTION__, __LINE__)
+
+/****************************************************************************/
+
+void db_catch_fn(sqlite3 *db, char *sql, const char *function, int line){
   char *zErrMsg=0;
   int rc;
 
-  rc = sqlite3_exec(db, cmd, 0, 0, &zErrMsg);
+  rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
   if( rc==SQLITE_OK ){
-    printf("FAIL: expected error was not generated\n\tsql: %s\n", cmd);
+    print_error(rc, "expected error was not generated", sql, function, line);
     sqlite3_free(zErrMsg);
-    exit(1);
+    QUIT_TEST();
   }
 
 }
 
-void db_execute_many(char *cmd, product *list, int count){
+#define db_catch(sql) db_catch_fn(db, sql, __FUNCTION__, __LINE__)
+
+/****************************************************************************/
+
+void db_execute_many_fn(sqlite3 *db, char *sql, char *types, int count,
+  const char *function, int line, ...
+){
+  va_list args;
   sqlite3_stmt *stmt=0;
-  char *zErrMsg=0; // *zTail=0;
+  char *zErrMsg=0;
   int rc, i;
 
-  rc = sqlite3_prepare(db, cmd, -1, &stmt, NULL);  //&zTail);
+  db_execute_fn(db, "BEGIN", function, line);
+
+  rc = sqlite3_prepare(db, sql, -1, &stmt, NULL);
   if( rc!=SQLITE_OK ){
-    printf("FAIL %d: %s\n\tsql: %s\n", rc, zErrMsg, cmd);
+    print_error(rc, zErrMsg, sql, function, line);
     sqlite3_free(zErrMsg);
-    exit(1);
+    QUIT_TEST();
   }
 
-  //if( sqlite3_bind_parameter_count(stmt)!=3 ){
-  //  printf("FAIL: statement uses %d parameters\n\tsql: %s\n", sqlite3_bind_parameter_count(stmt), cmd);
-  //  exit(1);
-  //}
+  va_start(args, line);
 
   for( i=0; i<count; i++ ){
-    sqlite3_bind_text(stmt, 2, list[i].name, -1, SQLITE_STATIC);
-    sqlite3_bind_int(stmt, 3, list[i].qty);
-    sqlite3_bind_double(stmt, 4, list[i].price);
+    rc = bind_sql_parameters(stmt, types, args);
+    if( rc!=SQLITE_OK ){
+      print_error(rc, "bind parameters", sql, function, line);
+      QUIT_TEST();
+    }
 
     rc = sqlite3_step(stmt);
     if( rc!=SQLITE_DONE ){
-      printf("FAIL %d: on step\nsql: %s\n", rc, cmd);
-      exit(1);
+      print_error(rc, "sqlite3_step", sql, function, line);
+      QUIT_TEST();
     }
 
     rc = sqlite3_reset(stmt);
     if( rc!=SQLITE_OK ){
-      printf("FAIL %d: on reset\nsql: %s\n", rc, cmd);
-      exit(1);
+      print_error(rc, "sqlite3_reset", sql, function, line);
+      QUIT_TEST();
     }
   }
 
+  va_end(args);
+
   sqlite3_finalize(stmt);
+
+  db_execute_fn(db, "COMMIT", function, line);
 
 }
 
-void db_check_int(char *cmd, int expected){
+#define db_execute_many(sql,types,count,...) db_execute_many_fn(db, sql, types, count, __FUNCTION__, __LINE__, __VA_ARGS__)
+
+/****************************************************************************/
+
+void db_check_int_fn(sqlite3 *db, char *sql, int expected, const char *function, int line){
   sqlite3_stmt *stmt=0;
   const char *zTail=0;
   int rc, returned;
 
   do{
 
-    rc = sqlite3_prepare(db, cmd, -1, &stmt, &zTail);
+    rc = sqlite3_prepare(db, sql, -1, &stmt, &zTail);
     if( rc!=SQLITE_OK ){
-      printf("FAIL %d: prepare\n\tsql: %s\n", rc, cmd);
-      exit(1);
+      print_error(rc, "sqlite3_prepare", sql, function, line);
+      QUIT_TEST();
     }
 
     rc = sqlite3_step(stmt);
@@ -96,57 +161,124 @@ void db_check_int(char *cmd, int expected){
     if( zTail && zTail[0] ){
 
       if( rc!=SQLITE_DONE ){
-        printf("FAIL %d: multi command returned a row\nsql: %s\n", rc, cmd);
-        exit(1);
+        print_error(rc, "multi command returned a row", sql, function, line);
+        QUIT_TEST();
       }
 
-      cmd = (char*) zTail;
+      sql = (char*) zTail;
 
     } else {
 
       if( rc!=SQLITE_ROW ){
-        printf("FAIL %d: no row returned\nsql: %s\n", rc, cmd);
-        exit(1);
+        print_error(rc, "no row returned", sql, function, line);
+        QUIT_TEST();
       }
 
       if( sqlite3_column_count(stmt)!=1 ){
-        printf("FAIL: returned %d columns\n\tsql: %s\n", sqlite3_column_count(stmt), cmd);
-        exit(1);
+        printf("\n\tFAIL: returned %d columns\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", sqlite3_column_count(stmt), sql, function, line);
+        QUIT_TEST();
       }
 
       returned = sqlite3_column_int(stmt, 0);
       if( returned!=expected ){
-        printf("FAIL: returned=%d expected=%d\n\tsql: %s\n", returned, expected, cmd);
-        exit(1);
+        printf("\n\tFAIL: expected=%d returned=%d\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", expected, returned, sql, function, line);
+        QUIT_TEST();
       }
 
       rc = sqlite3_step(stmt);
       if( rc!=SQLITE_DONE ){
-        printf("FAIL: additional row returned\n\tsql: %s\n", cmd);
-        exit(1);
+        printf("\n\tFAIL: additional row returned\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", sql, function, line);
+        QUIT_TEST();
       }
 
-      cmd = NULL;
+      sql = NULL;
 
     }
 
     sqlite3_finalize(stmt);
 
-  } while( cmd );
+  } while( sql );
 
 }
 
-void db_check_empty(char *cmd){
+#define db_check_int(sql,expected) db_check_int_fn(db, sql, expected, __FUNCTION__, __LINE__)
+
+/****************************************************************************/
+
+void db_check_str_fn(sqlite3 *db, char *sql, char *expected, const char *function, int line){
+  sqlite3_stmt *stmt=0;
+  const char *zTail=0;
+  char *returned;
+  int rc;
+
+  do{
+
+    rc = sqlite3_prepare(db, sql, -1, &stmt, &zTail);
+    if( rc!=SQLITE_OK ){
+      print_error(rc, "sqlite3_prepare", sql, function, line);
+      QUIT_TEST();
+    }
+
+    rc = sqlite3_step(stmt);
+
+    if( zTail && zTail[0] ){
+
+      if( rc!=SQLITE_DONE ){
+        print_error(rc, "multi command returned a row", sql, function, line);
+        QUIT_TEST();
+      }
+
+      sql = (char*) zTail;
+
+    } else {
+
+      if( rc!=SQLITE_ROW ){
+        print_error(rc, "no row returned", sql, function, line);
+        QUIT_TEST();
+      }
+
+      if( sqlite3_column_count(stmt)!=1 ){
+        printf("\n\tFAIL: returned %d columns\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", sqlite3_column_count(stmt), sql, function, line);
+        QUIT_TEST();
+      }
+
+      returned = (char*) sqlite3_column_text(stmt, 0);
+      if( strcmp(returned,expected)!=0 ){
+        printf("\n\tFAIL: expected='%s' returned='%s'\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", expected, returned, sql, function, line);
+        QUIT_TEST();
+      }
+
+      rc = sqlite3_step(stmt);
+      if( rc!=SQLITE_DONE ){
+        printf("\n\tFAIL: additional row returned\n\tsql: %s\n\tfunction: %s\n\tline: %d\n", sql, function, line);
+        QUIT_TEST();
+      }
+
+      sql = NULL;
+
+    }
+
+    sqlite3_finalize(stmt);
+
+  } while( sql );
+
+}
+
+#define db_check_str(sql,expected) db_check_str_fn(db, sql, expected, __FUNCTION__, __LINE__)
+
+/****************************************************************************/
+
+void db_check_empty_fn(sqlite3 *db, char *sql, const char *function, int line){
   sqlite3_stmt *stmt=0;
   const char *zTail=0;
   int rc;
 
   do{
 
-    rc = sqlite3_prepare(db, cmd, -1, &stmt, &zTail);
+    rc = sqlite3_prepare(db, sql, -1, &stmt, &zTail);
     if( rc!=SQLITE_OK ){
-      printf("FAIL %d: prepare\n\tsql: %s\n", rc, cmd);
-      exit(1);
+      print_error(rc, "sqlite3_prepare", sql, function, line);
+      QUIT_TEST();
     }
 
     rc = sqlite3_step(stmt);
@@ -154,28 +286,33 @@ void db_check_empty(char *cmd){
     if( zTail && zTail[0] ){
 
       if( rc!=SQLITE_DONE ){
-        printf("FAIL %d: multi command returned a row\nsql: %s\n", rc, cmd);
-        exit(1);
+        print_error(rc, "multi command returned a row", sql, function, line);
+        QUIT_TEST();
       }
 
-      cmd = (char*) zTail;
+      sql = (char*) zTail;
 
     } else {
 
       if( rc==SQLITE_ROW ){
-        printf("FAIL: unexpected row returned\nsql: %s\n", cmd);
-        exit(1);
+        print_error(rc, "unexpected row returned", sql, function, line);
+        QUIT_TEST();
       }
 
-      cmd = NULL;
+      sql = NULL;
 
     }
 
     sqlite3_finalize(stmt);
 
-  } while( cmd );
+  } while( sql );
 
 }
+
+#define db_check_empty(sql) db_check_empty_fn(db, sql, __FUNCTION__, __LINE__)
+
+/****************************************************************************/
+/****************************************************************************/
 
 int main(){
   int rc;
@@ -273,18 +410,17 @@ int main(){
 
   // another sale
 
-  product prod_list[3] = {
-      {"aaa", 5, 3.5},
-      {"bbb", 1, 2.99},
-      {"ccc", 2, 2.49}
-  };
-
   db_execute("insert into sales (datetime) values (datetime('now','localtime'))");
   db_execute("set @sale_id = last_insert_rowid()");
 
   db_check_int("select @sale_id", 3);
 
-  db_execute_many("insert into sale_items (sale_id,name,qty,price) values (@sale_id, ?, ?, ?)", prod_list, 3);
+  db_execute_many("insert into sale_items (sale_id,name,qty,price) values (@sale_id, ?, ?, ?)",
+                  "@sid", 3,
+                  "aaa", 5, 3.5,
+                  "bbb", 1, 2.99,
+                  "ccc", 2, 2.49
+  );
 
   db_check_int("select count(*) from sale_items", 7);
 
